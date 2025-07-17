3de1e51ebdd642e756e7a4fe6f898cece049b543
ioexcept <{ID}+{username}@users.noreply.github.com>
2025-07-17 12:31:10 +0000

    build scripts and docker-compose are working prelimarily. Testing to ensue

--- build.sh
+++ build.sh
@@ -2,8 +2,21 @@
 #for netbox: docker compose exec netbox /opt/netbox/netbox/manage.py createsuperuser
 
 
-cd netbox-docker/netbox-docker/
+cd nginx
+docker build -t nginx-landing .
+cd ..
+
+cd attack-navigator
+git clone https://github.com/mitre-attack/attack-navigator.git
+cd attack-navigator
+docker build -t attnav:prod .
+cd ..
+pwd
+cd ..
+pwd
+
+cd netbox-docker/
 docker pull netboxcommunity/netbox:v4.3-3.3.0
 docker tag netboxcommunity/netbox:v4.3-3.3.0 netbox:prod
-cd ../../
+cd ../
 
